924bb465d425ddeb3864224d9bfbfd45b4812e4f
Owen Rubel <orubel@gmail.com>
2023-03-17 08:05:25 -0700

    adding new config folder

--- boot.sql
+++ boot.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.16  Distrib 10.1.48-MariaDB, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.19  Distrib 10.2.44-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: devrank
+-- Host: localhost    Database: boot
 -- ------------------------------------------------------
--- Server version	10.1.48-MariaDB-0ubuntu0.18.04.1
+-- Server version	10.2.44-MariaDB-1:10.2.44+maria~bionic
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -15,8 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
-
+--
+-- Table structure for table `arch`
+--
 
 DROP TABLE IF EXISTS `arch`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -34,7 +35,19 @@ CREATE TABLE `arch` (
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `arch`
+--
 
+LOCK TABLES `arch` WRITE;
+/*!40000 ALTER TABLE `arch` DISABLE KEYS */;
+INSERT INTO `arch` VALUES (1,0,'\0','http://test.nosegrind.net:8080',NULL,'');
+/*!40000 ALTER TABLE `arch` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `authentication_token`
+--
 
 DROP TABLE IF EXISTS `authentication_token`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -48,7 +61,119 @@ CREATE TABLE `authentication_token` (
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `authentication_token`
+--
 
+LOCK TABLES `authentication_token` WRITE;
+/*!40000 ALTER TABLE `authentication_token` DISABLE KEYS */;
+INSERT INTO `authentication_token` VALUES (1,'2021-06-03 12:27:38','tpktuujh38ftebgrfourftjergt6mhn0','test');
+/*!40000 ALTER TABLE `authentication_token` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `authority`
+--
+
+DROP TABLE IF EXISTS `authority`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authority` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `authority` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=796339 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authority`
+--
+
+LOCK TABLES `authority` WRITE;
+/*!40000 ALTER TABLE `authority` DISABLE KEYS */;
+INSERT INTO `authority` VALUES (4,'ROLE_ADMIN'),(5,'ROLE_USER');
+/*!40000 ALTER TABLE `authority` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `branch`
+--
+
+DROP TABLE IF EXISTS `branch`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `branch` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `company_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `company_id` (`company_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1403 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `branch`
+--
+
+LOCK TABLES `branch` WRITE;
+/*!40000 ALTER TABLE `branch` DISABLE KEYS */;
+/*!40000 ALTER TABLE `branch` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1556 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `company`
+--
+
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dept`
+--
+
+DROP TABLE IF EXISTS `dept`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dept` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `branch_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `branch_id` (`branch_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1405 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dept`
+--
+
+LOCK TABLES `dept` WRITE;
+/*!40000 ALTER TABLE `dept` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dept` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hook`
+--
 
 DROP TABLE IF EXISTS `hook`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -66,12 +191,22 @@ CREATE TABLE `hook` (
   `attempts` bigint(20) NOT NULL,
   `format` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKaihuikmldahp68gmealxg88n5` (`user_id`),
-  CONSTRAINT `FKaihuikmldahp68gmealxg88n5` FOREIGN KEY (`user_id`) REFERENCES `person` (`id`)
+  KEY `FKaihuikmldahp68gmealxg88n5` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `hook`
+--
 
+LOCK TABLES `hook` WRITE;
+/*!40000 ALTER TABLE `hook` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hook` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hook_role`
+--
 
 DROP TABLE IF EXISTS `hook_role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -85,13 +220,22 @@ CREATE TABLE `hook_role` (
   `role_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK4c4vis9cjei2l6c7rklveugxm` (`hook_id`),
-  KEY `FK8l9jmkpllw3899fik0unk6h5g` (`role_id`),
-  CONSTRAINT `FK4c4vis9cjei2l6c7rklveugxm` FOREIGN KEY (`hook_id`) REFERENCES `hook` (`id`),
-  CONSTRAINT `FK8l9jmkpllw3899fik0unk6h5g` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+  KEY `FK8l9jmkpllw3899fik0unk6h5g` (`role_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `hook_role`
+--
 
+LOCK TABLES `hook_role` WRITE;
+/*!40000 ALTER TABLE `hook_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hook_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -115,8 +259,49 @@ CREATE TABLE `user` (
   `password_expired` bit(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_fwmwi44u55bo4rvwsv0cln012` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (19,4067,'admin','$2a$10$iBPrmLJZ5ZICscBO/0Nu3uLRAHHzYvsRHG/ObfHyM4lJnTsdSg.1q',NULL,'null','null','email@yourdomain.com','\0',NULL,NULL,NULL,'','\0','\0','\0'),(20,4067,'test','$2a$10$SyG6XhlW8JmM8CgV/cX.D.N6vEB8/xbEE3UKObUyzz7bIhYQlygHG',NULL,'null','null','test@yourdomain.com','\0',NULL,NULL,NULL,'','\0','\0','\0');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_authority`
+--
+
+DROP TABLE IF EXISTS `user_authority`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_authority` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `authority_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `authority_id` (`authority_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_authority`
+--
+
+LOCK TABLES `user_authority` WRITE;
+/*!40000 ALTER TABLE `user_authority` DISABLE KEYS */;
+INSERT INTO `user_authority` VALUES (1,19,4),(2,20,5);
+/*!40000 ALTER TABLE `user_authority` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_ip`
+--
 
 DROP TABLE IF EXISTS `user_ip`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -130,66 +315,26 @@ CREATE TABLE `user_ip` (
   `valid` bit(1) NOT NULL,
   `hash` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKe9kxf22cy0qh8yop6qf2xfabi` (`user_id`),
-  CONSTRAINT `FKe9kxf22cy0qh8yop6qf2xfabi` FOREIGN KEY (`user_id`) REFERENCES `person` (`id`)
+  KEY `FKe9kxf22cy0qh8yop6qf2xfabi` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `user_ip`
+--
 
-DROP TABLE IF EXISTS `user_authority`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_authority` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) NOT NULL,
-  `authority_id` bigint(20) NOT NULL,
-  `version` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  FOREIGN KEY (`authority_id`) REFERENCES `authority` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
-
-DROP TABLE IF EXISTS `authority`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `authority` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `version` bigint(20) NOT NULL,
-  `authority` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
-CREATE TABLE `company` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `version` bigint(20) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `branch` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `version` bigint(20) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `company_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`company_id`) REFERENCES `company` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `dept` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `version` bigint(20) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `branch_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`branch_id`) REFERENCES `branch` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
-
+LOCK TABLES `user_ip` WRITE;
+/*!40000 ALTER TABLE `user_ip` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_ip` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2022-12-05  9:46:07
